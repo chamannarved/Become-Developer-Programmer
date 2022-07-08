921d61255d00bb4549f7508e799b525188608a64
chamannarved <chamannarved@gmail.com>
2022-07-08 15:24:33 +0530

    Add basic fuctions of a expense tracker app

---
+++ Level 1/Expense Tracker/script.js
@@ -0,0 +1,61 @@
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
+const list = document.getElementById("transactions");
+const form = document.getElementById("form");
+
+const localStorageTransactions = JSON.parse(
+    localStorage.getItem("transactions")
+);
+
+let transactions =
+    localStorage.getItem("transactions") !== null
+        ? localStorageTransactions
+        : [];
+
+function addTransationToDOM(transaction) {
+    const sign = transaction.amount < 0 ? "-" : "+";
+    const liItem = document.createElement("li");
+
+    liItem.innerHTML = `${transaction.text} ${sign}${transaction.amount}`;
+    list.appendChild(liItem);
+}
+
+function genrateID() {
+    return Math.floor(Math.random() * 100000);
+}
+
+function updateLocalStorage() {
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+function init() {
+    list.innerHTML = "";
+    transactions.forEach(addTransationToDOM);
+    // updateValues();
+}
+
+function addTransaction(e) {
+    e.preventDefault();
+
+    if (text.value.trim() === "" || amount.value.trim() === "") {
+        alert("please add Text and Amount");
+    } else {
+        const transaction = {
+            id: genrateID(),
+            text: text.value,
+            amount: amount.value,
+        };
+
+        transactions.push(transaction);
+
+        addTransationToDOM(transaction);
+        // updateValues();
+        updateLocalStorage();
+        text.value = "";
+        amount.value = "";
+    }
+}
+
+init();
+
+form.addEventListener("submit", addTransaction);
